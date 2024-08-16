32defbd252139c58948e195978caae9b9625126d
SasaPlazas <146489725+SasaPlazas@users.noreply.github.com>
2024-08-16 17:47:54 -0500

    Add files via upload

---
+++ Ejercicio1.js
@@ -0,0 +1,21 @@
+const homeWorks = homeworks.find (names) [
+
+    {
+        names: 'Math',
+        completed: false
+
+    },
+    
+    {
+        names: 'language',
+        completed: false
+    },
+
+    {
+        names: 'social',
+        completed: true
+    }
+]
+array.id = homeWorks (names, completed); 
+if( homeWorks === names) ()=> (completed= true); 
+else ( completed= false =null)
---
+++ ejercicio2.js
@@ -0,0 +1,25 @@
+
+const TheCities = ([names], [people]); {
+
+    { 
+        names: 'Cali';
+        people: '5';
+    };
+    {
+        names: 'Bogotá';
+        people:7;
+    }; 
+    {
+        names: 'Medellin'; 
+        people: 8 ;
+    }
+}
+function foundCities (TheCities){
+    return TheCities 
+};
+
+
+
+
+
+
